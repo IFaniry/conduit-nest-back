47559771b0dca60ac5b4f6d3bb3801bd82fd2bfa
RAMANOHISOA Tahina Faniry Willy <faniry.consulting@gmail.com>
2024-05-18 14:58:24 +0200

    install fastify and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_CONNECTION),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
   ],
   controllers: [],
   providers: [],
